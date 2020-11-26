48a929b53d841f063c4d076f8c6ed5a577b3635e
Jules Lasne <jules.lasne@gmail.com>
2020-11-26 20:20:36 +0100

    Added more types of events handled

--- pydocteur/__init__.py
+++ pydocteur/__init__.py
@@ -29,6 +29,35 @@ def process_incoming_payload():
         reviewer = payload["requested_reviewer"]["login"]
         requester = payload["sender"]["login"]
         print(f"{requester} requested review from {reviewer} on PR #{pr_number}")
+    elif action == "created":
+        run_number = payload["check_run"]["id"]
+        status = payload["check_run"]["status"]
+        print(f"Check run {run_number} started with status {status}")
+    elif action == "completed":
+        if "check_run" in payload:
+            run_number = payload["check_run"]["id"]
+            conclusion = payload["check_run"]["conclusion"]
+            status = payload["check_run"]["status"]
+            print(f"Check run {run_number} completed with status {status} and conclusion {conclusion}")
+        elif "check_suite" in payload:
+            suite_number = payload["check_suite"]["id"]
+            conclusion = payload["check_suite"]["conclusion"]
+            status = payload["check_suite"]["status"]
+            print(f"Check suite {suite_number} completed with status {status} and conclusion {conclusion}")
+        else:
+            print(f"Keyerror on {action}: \n\n\n\n----------------------------{request.data}----------------------------\n\n\n\n")
+    elif action == "closed":
+        pr_number = payload["pull_request"]["number"]
+        closer = payload["sender"]["login"]
+        print(f"PR #{pr_number} was closed by {closer}")
+    elif action == "reopened":
+        pr_number = payload["pull_request"]["number"]
+        opener = payload["sender"]["login"]
+        print(f"PR #{pr_number} was reopened by {opener}")
+    elif action == "edited":
+        pr_number = payload["pull_request"]["number"]
+        user = payload["pull_request"]["user"]["login"]
+        print(f"PR #{pr_number} was edited by {user}: {payload['changes']}")
     else:
         print(f"Unknown action {action}: \n\n\n\n{request.data}\n\n\n\n")
     return "OK", 200
