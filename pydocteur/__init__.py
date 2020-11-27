410e8fc64845c5461c9ddf0a643880a751469483
Vincent Poulailleau <vpoulailleau@gmail.com>
2020-11-27 08:09:59 +0100

    refactor payload decoding

--- pydocteur/__init__.py
+++ pydocteur/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask, request
 import json
-from github import Github
 import os
+from contextlib import suppress
+
 from dotenv import load_dotenv
+from flask import Flask, request
+from github import Github
 
 load_dotenv()
 application = Flask(__name__)
@@ -10,66 +12,67 @@ application = Flask(__name__)
 gh = Github(os.getenv("GH_TOKEN"))
 
 
-@application.route('/', methods=['POST'])
+@application.route("/", methods=["POST"])
 def process_incoming_payload():
     payload = json.loads(request.data)
     try:
         action = payload["action"]
     except KeyError:
         return "OK", 200
+
+    with suppress(KeyError):
+        label_name = payload["label"]["name"]
+    with suppress(KeyError):
+        sender = payload["sender"]["login"]
+    with suppress(KeyError):
+        pr_number = payload["pull_request"]["number"]
+    with suppress(KeyError):
+        user = payload["review"]["user"]["login"]
+    with suppress(KeyError):
+        run_number = payload["check_run"]["id"]
+
     if action == "unlabeled":
-        label_name = payload["label"]["name"]
-        sender = payload["sender"]["login"]
-        pr_number = payload["pull_request"]["number"]
-        print(f"Label {label_name} was removed by {sender} on merge request #{pr_number}")
+        print(
+            f"Label {label_name} was removed by {sender} on merge request #{pr_number}"
+        )
     elif action == "labeled":
-        label_name = payload["label"]["name"]
-        sender = payload["sender"]["login"]
-        pr_number = payload["pull_request"]["number"]
         print(f"Label {label_name} was added by {sender} on merge request #{pr_number}")
     elif action == "submitted":
-        pr_number = payload["pull_request"]["number"]
         state = payload["review"]["state"]
-        user = payload["review"]["user"]["login"]
         message = payload["review"]["body"]
         print(f"PR #{pr_number} was {state} by {user} with message {message}")
     elif action == "review_requested":
-        pr_number = payload["pull_request"]["number"]
         reviewer = payload["requested_reviewer"]["login"]
         requester = payload["sender"]["login"]
         print(f"{requester} requested review from {reviewer} on PR #{pr_number}")
     elif action == "created":
-        run_number = payload["check_run"]["id"]
         status = payload["check_run"]["status"]
         print(f"Check run {run_number} started with status {status}")
     elif action == "completed":
         if "check_run" in payload:
-            run_number = payload["check_run"]["id"]
             conclusion = payload["check_run"]["conclusion"]
             status = payload["check_run"]["status"]
-            print(f"Check run {run_number} completed with status {status} and conclusion {conclusion}")
+            print(
+                f"Check run {run_number} completed with status {status} and conclusion {conclusion}"
+            )
         elif "check_suite" in payload:
             suite_number = payload["check_suite"]["id"]
             conclusion = payload["check_suite"]["conclusion"]
             status = payload["check_suite"]["status"]
-            print(f"Check suite {suite_number} completed with status {status} and conclusion {conclusion}")
+            print(
+                f"Check suite {suite_number} completed with status {status} and conclusion {conclusion}"
+            )
         else:
-            print(f"Keyerror on {action}: \n\n\n\n----------------------------{request.data}----------------------------\n\n\n\n")
+            print(
+                f"Keyerror on {action}: \n\n\n\n----------------------------{request.data}----------------------------\n\n\n\n"
+            )
     elif action == "closed":
-        pr_number = payload["pull_request"]["number"]
-        closer = payload["sender"]["login"]
-        print(f"PR #{pr_number} was closed by {closer}")
+        print(f"PR #{pr_number} was closed by {sender}")
     elif action == "reopened":
-        pr_number = payload["pull_request"]["number"]
-        opener = payload["sender"]["login"]
-        print(f"PR #{pr_number} was reopened by {opener}")
+        print(f"PR #{pr_number} was reopened by {sender}")
     elif action == "edited":
-        pr_number = payload["pull_request"]["number"]
-        user = payload["pull_request"]["user"]["login"]
         print(f"PR #{pr_number} was edited by {user}: {payload['changes']}")
     elif action == "opened":
-        pr_number = payload["pull_request"]["number"]
-        user = payload["pull_request"]["user"]["login"]
         print(f"PR #{pr_number} was opened by {user}")
     else:
         print(f"Unknown action {action}: \n\n\n\n{request.data}\n\n\n\n")
