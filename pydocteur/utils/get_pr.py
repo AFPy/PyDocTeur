06ec26db3872735fd2e1499c0a7cb52fa69d08fe
Jules Lasne <jules.lasne@gmail.com>
2020-11-29 18:35:45 +0100

    Added ignoring of checks for now

--- pydocteur/utils/get_pr.py
+++ pydocteur/utils/get_pr.py
@@ -8,28 +8,25 @@ def get_pull_request(gh, payload):
     logging.debug("Getting repository")
     gh_repo = gh.get_repo(os.getenv("REPOSITORY_NAME"))
     logging.info("Trying to find PR number from payload")
+
+    if payload["action"] == "completed":
+        logging.info("Check completed, ignoring and returning None")
+        return None
+
     try:
         try:
             pr_number = payload["pull_request"]["number"]
             logging.debug(f"Found PR {pr_number} first try")
         except KeyError:
+            issue_number = payload["issue"]["number"]
+            logging.debug(f"Found issue {issue_number} from payload")
             try:
-                pr_number = payload["check_run"]["pull_requests"][0]["number"]
-                logging.debug(f"Found PR {pr_number} second try")
-            except KeyError:
-                try:
-                    pr_number = payload["check_suite"]["pull_requests"][0]["number"]
-                    logging.debug(f"Found PR {pr_number} third try")
-                except KeyError:
-                    issue_number = payload["issue"]["number"]
-                    logging.debug(f"Found issue {issue_number} from payload")
-                    try:
-                        repo = gh_repo.get_pull(issue_number)
-                        logging.info(f"Found PR #{repo.number}")
-                        return repo
-                    except GithubException:
-                        logging.debug(f"Found issue {issue_number}, returning None")
-                        return None
+                repo = gh_repo.get_pull(issue_number)
+                logging.info(f"Found PR #{repo.number}")
+                return repo
+            except GithubException:
+                logging.debug(f"Found issue {issue_number}, returning None")
+                return None
     except:  # noqa
         logging.warning("Unknown payload, returning None")
         logging.debug(payload)
