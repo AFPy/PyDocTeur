8ae888bbddf671950347e776cfbd2af99eaf5bd1
Jules Lasne <jules.lasne@gmail.com>
2020-12-01 15:21:31 +0100

    Added small test for rest api

--- tests/test_0.py
+++ tests/test_0.py
@@ -1,2 +1,10 @@
-def test_0():
-    assert True
+import pytest
+
+from pydocteur.github_api import get_rest_api
+
+
+@pytest.mark.vcr()
+def test_get_rest_api():
+    resp = get_rest_api("https://api.github.com/repos/afpy/pydocteur")
+    data = resp.json()
+    assert data["name"] == "PyDocTeur"
