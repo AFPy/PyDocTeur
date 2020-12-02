8ba69d77f820b484777266a61a6ef800b976d017
Jules Lasne <jules.lasne@gmail.com>
2020-12-02 23:48:32 +0100

    Parametrized tests

--- tests/test_pr_status.py
+++ tests/test_pr_status.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from github import Github
 
@@ -5,25 +7,14 @@ from pydocteur.pr_status import is_label_set
 from pydocteur.pr_status import is_pr_approved
 from pydocteur.pr_status import is_pr_tests_passed
 
+REPOSITORY_NAME = os.getenv("REPOSITORY_NAME")
+
 
 @pytest.mark.vcr()
-@pytest.mark.parametrize("pr_number, approved", [(1451, True), (1490, True), (1452, False)])
+@pytest.mark.parametrize("pr_number, approved", [(1451, True), (1490, True), (1452, False), (1478, False)])
 def test_is_pr_approved(pr_number, approved):
     gh = Github()
-    assert is_pr_approved(gh.get_repo("python/python-docs-fr").get_pull(pr_number)) is approved
-
-
-@pytest.mark.vcr()
-def test_is_pr_unapproved():
-    gh = Github()
-    pr1 = gh.get_repo("python/python-docs-fr").get_pull(1452)
-    pr2 = gh.get_repo("python/python-docs-fr").get_pull(1423)
-    pr3 = gh.get_repo("python/python-docs-fr").get_pull(1478)
-    pr4 = gh.get_repo("python/python-docs-fr").get_pull(1436)
-    assert not is_pr_approved(pr1)
-    assert not is_pr_approved(pr2)
-    assert not is_pr_approved(pr3)
-    assert not is_pr_approved(pr4)
+    assert is_pr_approved(gh.get_repo(REPOSITORY_NAME).get_pull(pr_number)) is approved
 
 
 @pytest.mark.vcr()
@@ -36,14 +27,14 @@ def test_is_pr_tests_failed():
 @pytest.mark.vcr()
 def test_is_pr_tests_success():
     gh = Github()
-    pr = gh.get_repo("python/python-docs-fr").get_pull(1485)
+    pr = gh.get_repo(REPOSITORY_NAME).get_pull(1485)
     assert is_pr_tests_passed(pr)
 
 
 @pytest.mark.vcr()
 def test_is_label_set():
     gh = Github()
-    pr = gh.get_repo("python/python-docs-fr").get_pull(1485)
+    pr = gh.get_repo(REPOSITORY_NAME).get_pull(1485)
     assert is_label_set(pr, "🤖 automerge")
     assert not is_label_set(pr, "DO NOT MERGE")
 
@@ -51,7 +42,7 @@ def test_is_label_set():
 # @pytest.mark.vcr()
 # def test_state():
 #     gh = Github()
-#     pr = gh.get_repo("python/python-docs-fr").get_pull(1485)
+#     pr = gh.get_repo(REPOSITORY_NAME).get_pull(1485)
 
 
 # COMMENTED BECAUSE GRAPHQL NEEDS AUTH
@@ -66,5 +57,5 @@ def test_is_label_set():
 # @pytest.mark.vcr()
 # def test_is_not_first_time_contributor():
 #     gh = Github()
-#     pr = gh.get_repo("python/python-docs-fr").get_pull(1452)
+#     pr = gh.get_repo(REPOSITORY_NAME).get_pull(1452)
 #     assert not is_first_time_contributor(pr)
