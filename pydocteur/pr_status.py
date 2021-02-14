6c56b0fc57a8369e4802cb47e8cec692a6e838cb
Jules Lasne <jules.lasne@gmail.com>
2021-02-14 17:17:04 +0100

    Making the search for correct PR title more lineant

--- pydocteur/pr_status.py
+++ pydocteur/pr_status.py
@@ -88,7 +88,7 @@ def is_already_greeted(pr):
 def is_title_ok(pr):
     if is_label_set(pr, "meta"):
         return True
-    if pr.title.startswith(("traduction de", "Traduction de", "Fix fuzzies in", "Fix fuzzies dans")) and re.findall(
+    if pr.title.startswith(("traduction de", "Traduction de", "Fix fuzzies in", "Fix fuzzies dans")) or re.findall(
         r"\\.po", pr.title
     ):
         return True
